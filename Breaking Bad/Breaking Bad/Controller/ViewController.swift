4f5056abad0e8b551cc8addb5713f8da66975d7b
glsabaton <glsabaton@outlook.com>
2022-09-30 18:41:05 +0300

    Add fetchCharacters API function and test it

---
+++ Breaking Bad/Breaking Bad/Controller/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Breaking Bad
+//
+//  Created by Mehmet Ali Kısacık on 30.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        fetchCharacters()
+    }
+
+    private func fetchCharacters() {
+        NetworkManager.shared.fetchCharacters { results in
+            switch results {
+            case .success(let result):
+                print(result)
+            case .failure(let failure):
+                print(failure.errorDescription ?? "")
+            }
+        }
+    }
+
+}
+
--- Breaking Bad/Breaking Bad/Network/NetworkManager.swift
+++ Breaking Bad/Breaking Bad/Network/NetworkManager.swift
@@ -6,9 +6,28 @@
 //
 
 import Foundation
+import Alamofire
 
 final class NetworkManager {
 
+    static let shared = NetworkManager()
+
+    private var components = URLComponents() {
+        didSet{
+            components.scheme = "https"
+            components.host = NetworkConstants.apiHostString
+        }
+    }
+
+    func fetchCharacters (completion: @escaping (Result<Characters,AFError>) -> Void ) {
+        components.path = "/api/characters"
+
+        if let urlString = components.string {
+            AF.request(urlString, method: .get).responseDecodable(of: Characters.self) { response in
+                completion(response.result)
+            }
+        }
+    }
 
 }
 
