d58d3a7a984b712ef9e52dd65100ea5480904f91
glsabaton <glsabaton@outlook.com>
2022-09-30 17:50:27 +0300

    Create project

---
+++ Breaking Bad/Breaking Bad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Breaking Bad
+//
+//  Created by Mehmet Ali Kısacık on 30.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
