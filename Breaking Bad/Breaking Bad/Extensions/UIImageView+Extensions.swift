521847a779a7cb1ddc057ea1a9b5e3d6a5e01f8a
glsabaton <glsabaton@outlook.com>
2022-10-03 00:59:33 +0300

    Add completion handler to setImageWithPath

--- Breaking Bad/Breaking Bad/Extensions/UIImageView+Extensions.swift
+++ Breaking Bad/Breaking Bad/Extensions/UIImageView+Extensions.swift
@@ -14,7 +14,14 @@ extension UIImageView {
     func setImageWithPath(imageURLString: String?) {
         if let imageURLString = imageURLString {
             let processor = DownsamplingImageProcessor(size: self.frame.size )
-            self.kf.setImage(with: URL(string: imageURLString ), options: [.processor(processor),.scaleFactor(UIScreen.main.scale),.transition(.fade(0.3)),.cacheOriginalImage] )
+            self.kf.setImage(with: URL(string: imageURLString ), options: [.processor(processor),.scaleFactor(UIScreen.main.scale),.transition(.fade(0.3)),.cacheOriginalImage]) { result in
+                switch result{
+                case .success(_):
+                    break
+                case .failure(_):
+                    self.image = UIImage(systemName: "questionmark")?.withTintColor(.black,renderingMode: .alwaysOriginal)
+                }
+            }
         }
     }
 }
