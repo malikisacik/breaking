0828c73a4aaf0fe8bc3599a2d4fde0d9e4b8c29e
glsabaton <glsabaton@outlook.com>
2022-10-03 03:07:21 +0300

    Delete unnecessary model attributes

--- Breaking Bad/Breaking Bad/Model/Character.swift
+++ Breaking Bad/Breaking Bad/Model/Character.swift
@@ -8,30 +8,8 @@
 import Foundation
 
 struct Character: Codable {
-    let charID: Int?
     let name: String?
-    let birthday: String?
-    let occupation: [String]?
     let img: String?
-    let status: Status?
-    let nickname: String?
-    let appearance: [Int]?
-    let portrayed: String?
-    let category: Category?
-    let betterCallSaulAppearance: [Int]?
-}
-
-enum Category: String, Codable {
-    case betterCallSaul = "Better Call Saul"
-    case breakingBad = "Breaking Bad"
-    case breakingBadBetterCallSaul = "Breaking Bad, Better Call Saul"
-}
-
-enum Status: String, Codable {
-    case alive = "Alive"
-    case deceased = "Deceased"
-    case presumedDead = "Presumed dead"
-    case unknown = "Unknown"
 }
 
 typealias Characters = [Character]
